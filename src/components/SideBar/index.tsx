17f36f0e45a3f312ff55d3804f7c16a1444426e9
Lee-Seungje <moondgod@gmail.com>
2023-05-11 16:07:23 +0900

    Add SideBar

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,37 @@
+import * as S from './style';
+import * as I from 'images';
+import Image from 'next/image';
+import { css } from '@emotion/react';
+
+const SideBar = () => {
+  return (
+    <S.SideContainer>
+      <S.LogoContainer>
+        <S.Logo>
+          <Image alt="" src={I.LogoIcon} />
+        </S.Logo>
+        <S.Title>Stack Kowwlege</S.Title>
+      </S.LogoContainer>
+      <S.Opctions>
+        <S.Opction
+          css={css`
+            width: 67px;
+          `}
+        >
+          <Image alt="" src={I.HomeIcon} />
+          <S.OpctionText>홈</S.OpctionText>
+        </S.Opction>
+        <S.Opction>
+          <Image alt="" src={I.CoinIcon} />
+          <S.OpctionText>상점</S.OpctionText>
+        </S.Opction>
+        <S.Opction>
+          <Image alt="" src={I.RankIcon} />
+          <S.OpctionText>랭킹</S.OpctionText>
+        </S.Opction>
+      </S.Opctions>
+    </S.SideContainer>
+  );
+};
+
+export default SideBar;
---
+++ src/images/index.tsx
@@ -0,0 +1,4 @@
+export { default as CoinIcon } from './CoinIcon.svg';
+export { default as HomeIcon } from './HomeIcon.svg';
+export { default as RankIcon } from './RankIcon.svg';
+export { default as LogoIcon } from './LogoIcon.svg';
