15e6e539de3bd067d85780537c51df5698d1fa58
Lee-Seungje <moondgod@gmail.com>
2023-05-15 23:26:55 +0900

    Update Task layout

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { Timer } from 'components';
+import { Text, Container } from './style';
 
 const Task = () => {
   return (
-    <>
+    <Container>
       <Timer />
-    </>
+      <Text>남은 시간 !</Text>
+    </Container>
   );
 };
 export default Task;
---
+++ src/app/task/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Text = styled.p`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+
+  color: #000000;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
